ef9a4c62873f17ed9372c1951e2ab81b1f4b53f5
atschalz <andrej.tschalzev@web.de>
2025-07-29 22:33:25 +0000

    Update model config script

--- scripts/run_load_model_config.py
+++ scripts/run_load_model_config.py
@@ -3,46 +3,33 @@ from autogluon.core.models import DummyModel
 from tabrepo.benchmark.experiment import AGModelBagExperiment, YamlExperimentSerializer
 from tabrepo.utils.config_utils import ConfigGenerator
 
-
 def get_config_generator(model_name):
     if model_name == 'LightGBM':
-        from tabrepo.models.lightgbm.generate_alt import gen_lightgbm_alt as config_generator
+        from tabrepo.models.lightgbm.generate import gen_lightgbm as config_generator
     elif model_name == 'TabICL':
         from tabrepo.models.tabicl.generate import gen_tabicl as config_generator
     elif model_name == 'TabDPT':    
         from tabrepo.models.tabdpt.generate import gen_tabdpt as config_generator
-    elif model_name == 'LightGBM_alt':
-        from tabrepo.models.lightgbm.generate_alt import gen_lightgbm_alt as config_generator
     elif model_name == 'KNN':   
         from tabrepo.models.knn.generate import gen_knn as config_generator
     elif model_name == 'FastAI':
         from tabrepo.models.fastai.generate import gen_fastai as config_generator
     elif model_name == 'ExtraTrees':
         from tabrepo.models.extra_trees.generate import gen_extratrees as config_generator
-    elif model_name == 'ExtraTrees_alt':
-        from tabrepo.models.extra_trees.generate_alt import gen_xt_alt as config_generator
     elif model_name == 'CatBoost':
         from tabrepo.models.catboost.generate import gen_catboost as config_generator
-    elif model_name == 'CatBoost_alt':  
-        from tabrepo.models.catboost.generate_alt import gen_catboost_alt as config_generator
     elif model_name == 'XGBoost':
         from tabrepo.models.xgboost.generate import gen_xgboost as config_generator
-    elif model_name == 'XGBoost_alt':
-        from tabrepo.models.xgboost.generate_alt import gen_xgboost_alt as config_generator
     elif model_name == 'NN_Torch':
         from tabrepo.models.nn_torch.generate import gen_nn_torch as config_generator
     elif model_name == 'RandomForest':
         from tabrepo.models.random_forest.generate import gen_randomforest as config_generator
-    elif model_name == 'RandomForest_alt':
-        from tabrepo.models.random_forest.generate_alt import gen_rf_alt as config_generator
     elif model_name == 'Linear':
         from tabrepo.models.lr.generate import gen_linear as config_generator
     elif model_name == 'TabPFNv2':
         from tabrepo.models.tabpfnv2.generate import gen_tabpfnv2 as config_generator
     elif model_name == 'RealMLP':
         from tabrepo.models.realmlp.generate import gen_realmlp as config_generator
-    elif model_name == 'RealMLP_alt':
-        from tabrepo.models.realmlp.generate_alt import gen_realmlp_alt as config_generator
     elif model_name == 'EBM':
         from tabrepo.models.ebm.generate import gen_ebm as config_generator
     elif model_name == 'TabM':
@@ -60,7 +47,8 @@ if __name__ == '__main__':
     n_random_configs = 200
     n_random_configs_baselines = 50
     include_dummy = False
-    preprocessor_name = 'default_csv'
+    preprocessor_name = 'numint'
+    config_suffix = 'numint'
 
     # Dummy (constant predictor)
     if include_dummy:
@@ -89,9 +77,9 @@ if __name__ == '__main__':
         else:
             raise AssertionError(f"Found multiple instances of experiment named {experiment.name}. All experiment names must be unique!")
 
-    YamlExperimentSerializer.to_yaml(experiments=experiments_all, path="configs_all_new.yaml")
+    YamlExperimentSerializer.to_yaml(experiments=experiments_all, path=f"configs_all_{config_suffix}.yaml")
 
     from tabrepo.models.automl import generate_autogluon_experiments
     experiments_autogluon = generate_autogluon_experiments()
 
-    YamlExperimentSerializer.to_yaml(experiments=experiments_autogluon, path="configs_autogluon_new.yaml")
+    YamlExperimentSerializer.to_yaml(experiments=experiments_autogluon, path=f"configs_autogluon_{config_suffix}.yaml")
